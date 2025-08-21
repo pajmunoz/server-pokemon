a17b29e0e565a28b4d469a8513471f3e2d75837b
Pablo Jara <pablojaramunoz@icloud.com>
2025-08-21 03:03:11 -0500

    test vercel

--- api/index.js
+++ api/index.js
@@ -3,3 +3,7 @@ const app = require('../index.js');
 
 // Exportar la app para Vercel
 module.exports = app;
+
+// Para debugging en Vercel
+console.log('API index.js cargado correctamente');
+console.log('App exportada:', typeof app);
